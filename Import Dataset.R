f2edc03cf1684b83f09f35f9b6df23056fdadda3
manuelfuguet <manuel.fuguet@aizon.ai>
2024-12-17 20:50:44 +0100

    En el codigo se ha modificado para que no tome una linea con contenido como header

--- Import Dataset.R
+++ Import Dataset.R
@@ -1,5 +1,5 @@
 # Leer archivo CSV en R
-data <- read.csv("/Users/manuel.fuguet/Downloads/epa-http.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+data <- read.csv("/Users/manuel.fuguet/Downloads/epa-http.csv", header = TRUE, sep = "/", stringsAsFactors = FALSE)
 
 # Ver los primeros datos
 head(data)
