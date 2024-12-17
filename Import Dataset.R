05544bb855d643e5b47e1ac1794d903a433d387b
manuelfuguet <manuel.fuguet@aizon.ai>
2024-12-17 21:27:44 +0100

    El codigo se ha modificado para agregar nuevas columnas

--- Import Dataset.R
+++ Import Dataset.R
@@ -1,5 +1,7 @@
-# Leer archivo CSV en R
-data <- read.csv("/Users/manuel.fuguet/Downloads/epa-http.csv", header = TRUE, sep = "/", stringsAsFactors = FALSE)
-
-# Ver los primeros datos
-head(data)
+# Leer archivo CSV
+data <- read.csv("/Users/manuel.fuguet/Downloads/epa-http.csv", 
+                 header = FALSE, sep = " ", stringsAsFactors = FALSE)
+library(readr)
+epa_http <- read_table("~/Downloads/epa-http.csv", 
+                       col_names = FALSE)
+View(epa_http)
