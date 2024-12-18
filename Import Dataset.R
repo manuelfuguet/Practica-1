eb1cd0902a1efe3e8ba92874fbbd095cbbaf3212
ThommasGM <andrewthommas98@gmail.com>
2024-12-18 07:54:18 -0300

    Data set + Preguntas practicas 1.1 y 1.2

--- Import Dataset.R
+++ Import Dataset.R
@@ -1,7 +1,33 @@
-# Leer archivo CSV
-data <- read.csv("/Users/manuel.fuguet/Downloads/epa-http.csv", 
-                 header = FALSE, sep = " ", stringsAsFactors = FALSE)
-library(readr)
-epa_http <- read_table("~/Downloads/epa-http.csv", 
-                       col_names = FALSE)
-View(epa_http)
+# Ver las primeras filas del dataframe 'epa_http'
+head(epa_http)
+
+# Verifica los nombres actuales de las columnas
+print(colnames(epa_http))
+
+# Cambiar los nombres de las columnas
+colnames(epa_http) <- c("IP", "Timestamp", "Request Type", "URL", "Protocol", "Response Code", "Bytes of Reply")
+
+# Verifica los nuevos nombres de las columnas
+print(colnames(epa_http))
+
+
+# Verificar los nombres actuales de las columnas
+print(colnames(epa_http))
+
+
+epa_http$`Bytes of Reply` <- gsub("-", NA, epa_http$`Bytes of Reply`)
+
+# Convertir la columna 'Bytes of Reply' a numérico
+epa_http$`Bytes of Reply` <- as.numeric(epa_http$`Bytes of Reply`)
+
+
+valor_medio <- mean(epa_http$`Bytes of Reply`, na.rm = TRUE)
+
+
+# Calcular las dimensiones del dataset
+dimensiones <- dim(epa_http)
+
+# Mostrar el resultado del promedio y las dimensiones del dataset
+
+print(paste("Promedio de 'Bytes of Reply':", valor_medio))
+print(paste("Dimensiones del dataset (Filas, Columnas):", paste(dimensiones, collapse = ", ")))
